6df13a42d3a14356a3ea0396e9a8e4d792d777cd
Alexander Ewetumo <trinoxf@gmail.com>
2016-02-27 13:22:21 +0100

    Added new initialization API background

--- animators/inits.go
+++ animators/inits.go
@@ -3,8 +3,8 @@ package animators
 import "github.com/influx6/govfx"
 
 func init() {
-	govfx.RegisterAnimator("translate-x", func(m govfx.Values) govfx.Sequence {
-		return gofx.Merge(TranslateX{}, m)
-	})
+
+	govfx.RegisterSequence("translate-x", TranslateX{})
+	govfx.RegisterSequence("translate-y", TranslateY{})
 
 }
--- engine.go
+++ engine.go
@@ -1,5 +1,12 @@
 package govfx
 
+import (
+	"errors"
+	"fmt"
+
+	"github.com/influx6/faux/reflection"
+)
+
 //==============================================================================
 
 // Animate provides the central engine for managing all animation calls.
@@ -72,7 +79,34 @@ func RegisterEasing(name string, easing Easing) {
 	easingProviders.Add(name, easing)
 }
 
-// RegisterAnimator adds a animator into the lists with a giving name, this can
+// NewSequence returns a new sequence tagged by the giving name, using the
+// values map to initialize the attributes accordingly, else returns an
+// error if the sequence name does not exists.
+func NewSequence(name string, m Values) (Sequence, error) {
+	ani := animationProviders.Get(name)
+	if ani == nil {
+		return nil, fmt.Errorf("No Sequence with Name[%s]", name)
+	}
+
+	return ani(m), nil
+}
+
+// RegisterSequence adds a sequence by taking a sample value type of the real struct
+// that provides that and generating a new one when requested.
+func RegisterSequence(name string, structType interface{}) error {
+	if reflection.IsStruct(structType) {
+		return errors.New("Not a Struct")
+	}
+
+	animationProviders.Add(name, func(m Values) Sequence {
+		newSeq, _ := reflection.MakeNew(structType)
+		return Merge(newSeq, m)
+	})
+
+	return nil
+}
+
+// RegisterAnimator adds a sequence into the lists with a giving name, this can
 // be retrieved later to build a animations lists from.
 func RegisterAnimator(name string, ani Animator) {
 	animationProviders.Add(name, ani)
